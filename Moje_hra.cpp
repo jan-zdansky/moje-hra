9b2396e178940965b855c016da1270cb6c8ccbb6
jan zdansky <janzda2009@gmail.com>
2026-05-12 17:27:18 +0200

    Struktury a pomocne funkce

--- Moje_hra.cpp
+++ Moje_hra.cpp
@@ -1,6 +1,32 @@
 #include <iostream>
+#include <vector>
+#include <ctime>
+#include <windows.h>
 
-int main() {
-    std::cout << "Ahoj, tohle je muj projekt v Gitu!" << std::endl;
-    return 0;
+using namespace std;
+
+// Pomocná funkce pro barvy v konzoli (Windows)
+void nastavBarvu(int barva) {
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), barva);
+}
+
+// Definice monster
+struct Monstrum {
+    string jmeno;
+    int zivoty;
+    int utok;
+    string hlaska;
+};
+
+// Definice hráèe a jeho statistik
+struct Hrac {
+    string classa;
+    int hp, maxHp, mana, maxMana, utok, gold, level, exp, expPotreba;
+};
+
+// Funkce pro zobrazení aktuálního stavu hráèe
+void ukazStav(Hrac h) {
+    nastavBarvu(7);
+    cout << "\n--- STAV [LVL " << h.level << "] | HP: " << h.hp << "/" << h.maxHp
+         << " | Mana: " << h.mana << " | Gold: " << h.gold << "g | EXP: " << h.exp << "/" << h.expPotreba << " ---" << endl;
 }
